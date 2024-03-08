6c4c1e8bd80bc1512d3e9461f9d765d5ee848459
pedro-0101m <pedro.tiete@outlook.com>
2024-03-08 20:21:10 -0300

    Exercicio 1 lista 2

---
+++ Aula_4_Classes/Lista_exercicios_2.js
@@ -0,0 +1,19 @@
+"use strict";
+class Carro_exe {
+    constructor(marca, modelo, ano, cor) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+    }
+    exibirInfo() {
+        console.log(`Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}, Cor: ${this.cor}`);
+    }
+    get GetIdade() {
+        let date = new Date().getFullYear();
+        return date - this.ano;
+    }
+}
+let Carro1 = new Carro_exe("Toyota", "Corolla", 2020, "Prata");
+console.log(Carro1.GetIdade);
+//meuCarro.exibirInfo();
